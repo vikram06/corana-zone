1533233523352b2bf9a80422bc655c80f1f152d4
Vikram.R <vikram.r@sysarcinfomatix.com>
2020-04-12 21:50:13 +0530

    server changes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/corana-zone'));
+app.use(express.static(__dirname + '/dist/safezone'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/corana-zone/index.html'));
+res.sendFile(path.join(__dirname+'/dist/safezone/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
